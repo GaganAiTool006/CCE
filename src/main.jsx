7df200180f402016ca209b823b74c301c3892ef0
gagan chouhan <gaganchouhan31494@gmail.com>
2026-08-22 07:47:15 -0700

    Fix production build CSS relative paths with base './' and ensure universal stylesheet imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './css/main.css';
+import './css/navbar.css';
+import './css/pages.css';
+import './css/auth.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
